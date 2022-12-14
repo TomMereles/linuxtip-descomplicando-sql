c080f25163c2bb20ab91d5b36bdc221f6dfc6328
TomMereles <TomMereles>
2022-12-14 00:27:11 +0000

    SELECT * FROM

---
+++ dia-01/from.sql
@@ -0,0 +1,23 @@
+-- Databricks notebook source
+SELECT * FROM silver_olist.pagamento_pedido
+
+-- COMMAND ----------
+
+SELECT
+  idPedido,
+  descSituacao
+FROM
+  silver_olist.pedido
+  
+  LIMIT 5
+
+-- COMMAND ----------
+
+SELECT *,
+       DATEDIFF(dtEstimativaEntrega, dtEntregue) AS qtDiasPromessaEntrega
+
+FROM silver_olist.pedido
+
+-- COMMAND ----------
+
+
